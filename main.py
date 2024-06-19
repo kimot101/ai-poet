c8a5dd318cdc9469f75733cd2f67e0c3b2975053
Kim Euntae <58654935+kimot101@users.noreply.github.com>
2024-06-19 23:00:46 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_openai import ChatOpenAI
+import streamlit as st
+import time
+
+llm = ChatOpenAI()
+
+st.title('인공지능 시인')
+content = st.text_input("시의 주제를 제시해주세요.")
+
+if st.button("시 작성 요청하기"):
+    with st.spinner('시 작성중...'):
+        result = llm.invoke(content + "에 대한 시를 써줘")
+        st.write("시")
+        st.write(result.content)
